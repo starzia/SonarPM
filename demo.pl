725ec215aed2b31bfe040ec48fb6a2eb7c93176a
Steve Tarzia <steve@stevetarzia.com>
2009-06-09 16:22:04 +0000

    updated plot demo

--- demo.pl
+++ demo.pl
@@ -3,6 +3,7 @@ $HIGHEST=0.01;
 $LOWEST=0.0;
 $LCD_STEPS=8;
 $MAXX=50;
+$WINDOW_SIZE=5;
 
 use FileHandle;
 
@@ -10,17 +11,26 @@ open(S,"./sonar --poll |");
 S->autoflush(1);
 open(F,">data.txt");
 F->autoflush(1);
+open(F2,">data2.txt");
+F2->autoflush(1);
 open(G,"|gnuplot");
 G->autoflush(1);
 
 $count=0;
+$window_sum=0;
 while (<S>) { 
     if (/delta:(.*)\}/) {
-	$count++;
-	backlight( $1 );
+	$window_sum += $1;
+	if( $count % $WINDOW_SIZE == 0 ){
+	    $avg = $window_sum/$WINDOW_SIZE;
+	    print F2 $count," ",$avg,"\n";
+	    backlight( $avg );
+	    $window_sum=0
+	}
 	print F $1, "\n";
 	print G sprintf( "set xrange [%d:%d]\n", $count-$MAXX,$count );
-	print G sprintf( "plot 'data.txt' with linespoints, (%f) w l, (%f) w l\n", $LOWEST, $HIGHEST);
+	print G sprintf( "plot 'data.txt' with linespoints lw 5, 'data2.txt' w linespoints lw 5, (%f) w l lw 3, (%f) w l lw 3\n", $LOWEST, $HIGHEST);
+	$count++;
     }
 }
 
